b73b706f284b5d9161343577c4297f82bfcd3402
threezool <threezool@gmail.com>
2015-12-13 20:30:29 +0100

    Working

---
+++ Helloworld/Helloworld.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Helloworld
+{
+    /// <summary>
+    /// Hello World! - Working
+    /// </summary>
+
+    internal class Helloworld
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
