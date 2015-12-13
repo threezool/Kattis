6760ab43134d5ec79db97068f313386a5044bbaa
threezool <threezool@gmail.com>
2015-12-13 18:02:51 +0100

    Added description for problem

--- Phonelist/phonelist.cs
+++ Phonelist/phonelist.cs
@@ -1,6 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 
+/// <summary>
+/// Phonelist - Not Working
+/// 
+/// Given a list of phone numbers, determine if it is consistent in the sense that no number is the prefix of another. 
+/// Let’s say the phone catalogue listed these numbers:
+///
+/// - Emergency 911
+/// - Alice 97 625 999
+/// - Bob 91 12 54 26
+///
+/// In this case, it’s not possible to call Bob, because the central would direct your call to the emergency line as soon 
+/// as you had dialled the first three digits of Bob’s phone number.So this list would not be consistent.
+/// </summary>
+
 namespace Kattis
 {
     class phonelist
