2a9817009d49023a8286b4735fb1048999a39730
threezool <threezool@gmail.com>
2015-12-13 18:44:58 +0100

    Working

--- Reversebinary/reversebinary.cs
+++ Reversebinary/reversebinary.cs
@@ -1,17 +1,17 @@
 ﻿using System;
 
-/// <summary>
-/// Reverse Binary - Working 
-/// 
-/// Yi has moved to Sweden and now goes to school here. The first years of schooling she got in China, and the curricula do not 
-/// match completely in the two countries. Yi likes mathematics, but now... The teacher explains the algorithm for subtraction on 
-/// the board, and Yi is bored. Maybe it is possible to perform the same calculations on the numbers corresponding to the reversed 
-/// binary representations of the numbers on the board? Yi dreams away and starts constructing a program that reverses the binary 
-/// representation, in her mind. As soon as the lecture ends, she will go home and write it on her computer.
-/// </summary>
-
 namespace Kattis
 {
+    /// <summary>
+    /// Reverse Binary - Working 
+    /// 
+    /// Yi has moved to Sweden and now goes to school here. The first years of schooling she got in China, and the curricula do not 
+    /// match completely in the two countries. Yi likes mathematics, but now... The teacher explains the algorithm for subtraction on 
+    /// the board, and Yi is bored. Maybe it is possible to perform the same calculations on the numbers corresponding to the reversed 
+    /// binary representations of the numbers on the board? Yi dreams away and starts constructing a program that reverses the binary 
+    /// representation, in her mind. As soon as the lecture ends, she will go home and write it on her computer.
+    /// </summary>
+    
     class reversebinary
     {
         static void Main(string[] args)
